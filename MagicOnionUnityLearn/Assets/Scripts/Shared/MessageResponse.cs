58b4a15ff78c4da5e90c51cb72549aca8e9ee828
haiun <gewdox@naver.com>
2021-03-30 18:49:54 +0900

    누락된 파일 커밋.

---
+++ MagicOnionUnityLearn/Assets/Scripts/Shared/MessageResponse.cs
@@ -0,0 +1,13 @@
+﻿using MessagePack;
+
+namespace GrpcService1.Shared
+{
+    [MessagePackObject]
+    public struct MessageResponse
+    {
+        [Key(0)]
+        public string UserName { get; set; }
+        [Key(1)]
+        public string Message { get; set; }
+    }
+}
